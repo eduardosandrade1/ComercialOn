8bc79c1f7f4da69d94267f5fbc64d05d8ce5870c
Eduardo Andrade <tvduardo@gmail.com>
2021-06-15 11:51:58 -0300

    inserindo métodos da classe cliente

--- ComercialOn/Classes/Cliente.cs
+++ ComercialOn/Classes/Cliente.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ComercialOn.Classes
 {
-    class Cliente
+    /// <summary>
+    /// Classe para executar as ações dos clientes
+    /// </summary>
+    public class Cliente
     {
+        // atributo da classe
+        public int Id { get; set; }
+        public string Nome { get;set }
+        // método contrutor tem o mesmo nome da classe
+        public Cliente(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        // métodos da classe
+        public void Inserir() 
+        {
+            // faz o processo de inserção no DB
+        }
+        public bool Alterar(int id, object list)
+        {
+            // faz o processo de update
+            return true; // Apenas para o itelisence
+        }
+        // lista todos os clientes
+        public static List<Cliente> ListarTodos()
+        {
+            List<Cliente> lista = new List<Cliente>();
+            // code para capturar infos no banco...
+            return lista;
+        }
+        // busca por id, mas não retorna os dados
+        public void BuscarPorId(int id)
+        {
+
+        }
     }
 }
